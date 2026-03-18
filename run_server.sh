59cee07a46b82e9b21a9f34a7807b8a66e9542e9
Seoiur <105098047+Seoiur@users.noreply.github.com>
2026-03-18 23:35:23 +0800

    fix local jekyll setup and update config

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,9 @@
-bundle exec jekyll liveserve
+#!/bin/zsh
+source ~/.zshrc
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+chruby ruby-3.1.4
+cd /Users/seoi/Documents/GitHub/Seoiur.github.io
+bundle _2.2.19_ exec jekyll serve --livereload
